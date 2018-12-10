e8333e4d919ef0dc882ee53ffc3b81b9fb1f183f
Adam Szofran <adam@visualvocal.com>
2018-12-10 13:52:01 -0800

    Add Visual Studio build.

---
+++ make-win64vs.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+path=%ProgramFiles%\CMake\bin;%ProgramFiles%\NASM;%path%
+if not exist build\win64vs mkdir build\win64vs
+pushd build\win64vs
+cmake -G"Visual Studio 15 2017 Win64" -DCMAKE_INSTALL_PREFIX=install -DLeptonica_DIR=..\leptonica\build\win64vs\install\cmake -DLeptonica_LIBRARY_DIRS=..\leptonica\build\win64vs\install\lib -DLeptonica_INCLUDE_DIRS=..\leptonica\build\win64vs\install\include\leptonica -DBUILD_TRAINING_TOOLS=OFF %~dp0
+popd
+endlocal
+
